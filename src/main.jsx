fed51f6cbc4c640c2e631a2848181d0727690ac4
Ngô Vưn Nam <anh02284@gmail.com>
2022-11-25 12:20:52 +0700

    install redux thunk

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
 );
