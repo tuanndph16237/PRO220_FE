eb6fee1a78f4ea38df40e2e93107fc9dacde0cd8
Nguyễn Xuân Phúc <phucnx1909@gmail.com>
2022-11-30 00:26:43 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './app/store';
-import "antd/dist/reset.css";
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
