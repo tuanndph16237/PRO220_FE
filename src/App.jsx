d1d643e65263f6a4cfa6afff8a691394c2d7928b
Ngô Vưn Nam <85476745+namnvph16599@users.noreply.github.com>
2022-11-29 09:03:29 +0700

    UI/main layout (#1)
    * ui: header main layout
    
    * ui: fake img avatar
    
    * ui: social
    
    * setting layout main
    
    * ui: carousel home page
    
    * fix: route login, register
    
    * ui:login, register page

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ import MainLayout from './layouts/main';
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
 import AdminLayout from './layouts/admin';
+import BookingPage from './pages/Booking';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
     return (
         <Routes>
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<HomePage />} />
                 <Route path="about" element={<AboutPage />} />
+                <Route path="dat-lich" element={<BookingPage />} />
+                <Route path="dang-nhap" element={<Login />} />
+                <Route path="dang-ky" element={<Register />} />
             </Route>
             <Route path="/admin" element={<AdminLayout />}>
                 <Route index element={<HomePage />} />
