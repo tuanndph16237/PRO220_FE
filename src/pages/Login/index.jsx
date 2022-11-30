4628a452c8148b2a3a0c9b20e3760bfb2889fa19
Nguyễn Xuân Phúc <phucnx1909@gmail.com>
2022-11-30 20:08:00 +0700

    ui: login-register-page-update

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,54 +1,56 @@
 import React from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 const Login = () => {
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
       };
     
       return (
-        
-        <Form
-          name="normal_login"
-          className="login-form w-[400px] m-auto"
-          initialValues={{ remember: false }}
-          onFinish={onFinish}
-        >
-          <h1 className="text-[28px] mb-6">Đăng nhập</h1>
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your Phone Number!' }]}
+        <div className="flex h-[100vh] items-center">
+          <Form
+            name="normal_login"
+            className="login-form w-[360px] m-auto"
+            initialValues={{ remember: false }}
+            onFinish={onFinish}
           >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Phone Number" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox className="float-left" >Remember me</Checkbox>
+            <h1 className="text-[28px] mb-6">Đăng nhập</h1>
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Vui lòng nhập Số điện thoại!' }]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Số điện thoại" />
             </Form.Item>
-    
-            <a className="login-form-forgot float-right" href="">
-              Quên mật khẩu?
-            </a>
-          </Form.Item>
-    
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button bg-[#02b875] w-full ">
-              Đăng nhập
-            </Button>
-            <p className="my-5 text-[#02b875] text-center">Chưa có tài khoản?</p>
-            <Button className="w-full">Đăng ký tài khoản</Button>
-          </Form.Item>
-        </Form>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Vui lòng nhập Mật khẩu!' }]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Mật khẩu"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox className="float-left" >Remember me</Checkbox>
+              </Form.Item>
+      
+              <a className="login-form-forgot float-right hover:text-[#1464f4]" href="">
+                Quên mật khẩu?
+              </a>
+            </Form.Item>
+      
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button bg-[#02b875] w-full ">
+                Đăng nhập
+              </Button>
+              <p className="my-5 text-center">Chưa có tài khoản?</p>
+              <Link to='/dang-ky' className="inline-block w-full py-1 rounded text-[#1464f4] text-center border border-[#1464f4] hover:text-[#fff] hover:bg-[#125eb9]">Đăng ký tài khoản</Link>
+            </Form.Item>
+          </Form>
+        </div>
       );
 };
 
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,81 +1,83 @@
 import React from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 const Register = () => {
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
       };
     
       return (
-        
-        <Form
-          name="normal_login"
-          className="login-form w-[400px] m-auto"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-        >
-          <h1 className="text-[28px] mb-6">Đăng ký tài khoản</h1>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: 'Please input your Name!' }]}
+        <div className="flex h-[100vh] items-center">
+          <Form
+            name="normal_login"
+            className="login-form w-[400px] m-auto"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
           >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Name" />
-          </Form.Item>
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your Phone Number!' }]}
-          >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Phone Number" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Repeat Password"
-            />
-          </Form.Item>
-          <Form.Item className="">
-            <b>Mật khẩu bao gồm</b>
-            <p className="mt-2">
-                <svg className="w-[14px] inline-block mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
-                Ít nhất 8 ký tự
-            </p>
-            <p className="my-2">
-                <svg className="w-[14px] inline-block mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
-                Chữ hoa & chữ thường
-            </p>
-            <p className="">
-                <svg className="w-[14px] inline-block mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
-                Ít nhất 1 số
-            </p>
-          </Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox className="mb-5" >Đăng ký nhận thông tin chương trình khuyến mãi, dịch vụ từ VinFast</Checkbox>
-          </Form.Item>
-          <Form.Item name="remember">
-            <p>Bằng việc bấm nút Đăng ký bên dưới, tôi xác nhận đã đọc, hiểu và đồng ý với các <a href="#" className="underline text-[#cfb4dd]">Điều kiện và Điều khoản</a> của VinFast.</p>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button bg-[#02b875] w-full ">
-              Đăng ký
-            </Button>
-            <p className="my-5 text-[#02b875] text-center">Đã có tài khoản?</p>
-            <Button className="w-full">Đăng nhập</Button>
-          </Form.Item>
-        </Form>
+            <h1 className="text-[28px] mb-6">Đăng ký tài khoản</h1>
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: 'Vui lòng nhập Họ và tên!' }]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Họ và tên" />
+            </Form.Item>
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Vui lòng nhập Số điện thoại!' }]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Số điện thoại" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Vui lòng nhập Mật khẩu!' }]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Mật khẩu"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Vui lòng nhập lại Mật khẩu!' }]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Nhập lại mật khẩu"
+              />
+            </Form.Item>
+            <Form.Item className="">
+              <b>Mật khẩu bao gồm</b>
+              <p className="mt-2">
+                  <svg className="w-[14px] inline-block mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                  Ít nhất 8 ký tự
+              </p>
+              <p className="my-2">
+                  <svg className="w-[14px] inline-block mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                  Chữ hoa & chữ thường
+              </p>
+              <p className="">
+                  <svg className="w-[14px] inline-block mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                  Ít nhất 1 số
+              </p>
+            </Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox className="mb-5" >Đăng ký nhận thông tin chương trình khuyến mãi, dịch vụ từ Dodoris</Checkbox>
+            </Form.Item>
+            <Form.Item name="remember">
+              <p>Bằng việc bấm nút Đăng ký bên dưới, tôi xác nhận đã đọc, hiểu và đồng ý với các <a href="#" className="underline text-[#cfb4dd]">Điều kiện và Điều khoản</a> của Dodoris.</p>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button bg-[#02b875] w-full ">
+                Đăng ký
+              </Button>
+              <p className="my-5 text-center">Đã có tài khoản?</p>
+              <Link to='/dang-nhap' className="inline-block w-full py-1 rounded text-[#1464f4] text-center border border-[#1464f4] hover:text-[#fff] hover:bg-[#125eb9]">Đăng nhập</Link>
+            </Form.Item>
+          </Form>
+        </div>
       );
 };
 
