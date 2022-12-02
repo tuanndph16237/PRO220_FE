49f1411ed09d250ce5db7f6ad7908ab02482ef16
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-02 10:01:44 +0700

    config change languege vi_VN antd

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import vi_VN from 'antd/locale/vi_VN';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ConfigProvider locale={vi_VN}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ConfigProvider>
     </Provider>,
 );
