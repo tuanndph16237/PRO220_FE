efc1a2207c07ed0d5bed170537fc6ad9cf2272b1
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-04 23:01:37 +0700

    add colot btn checkbox disable

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-    // Define a top-level state field named `todos`, handled by `todosReducer`
-});
-
-export default rootReducer;
