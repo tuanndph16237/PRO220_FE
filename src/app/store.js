eb40287482f778811fc546c6f195295e878c7a58
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-04 22:36:48 +0700

    refactor store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import { configureStore } from '@reduxjs/toolkit';
+import BannerReducer from '../slices/banner';
+const store = configureStore({
+    reducer: {
+        banner: BannerReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
 export default store;
