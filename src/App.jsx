2545fd202dcbb2c4232301e09ffbadf8a5da72d7
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-10 11:12:22 +0700

    feat: add route update banner

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import BannerManage from './pages/Admin/Banner';
 import PageNotFound from './pages/PageNotFound';
+import UpdateBanner from './pages/Admin/Banner/UpdateBanner';
 function App() {
     return (
         <Routes>
@@ -22,8 +23,8 @@ function App() {
                 <Route path="dang-ky" element={<Register />} />
             </Route>
             <Route path="/admin" element={<AdminLayout />}>
-                <Route path="dang-ky" element={<Register />} />
                 <Route path="quan-ly-banner" element={<BannerManage />} />
+                <Route path="quan-ly-banner/:id" element={<UpdateBanner />} />
             </Route>
             <Route path="*" element={<PageNotFound />} />
         </Routes>
