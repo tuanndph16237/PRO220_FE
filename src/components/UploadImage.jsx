76a5ac3a420f709744dfdb77f0958c0201c5f4dd
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-11 11:24:35 +0700

    feat: refactor cpn uploadimage

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
 import axios from 'axios';
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, Upload, notification } from 'antd';
-import { NOTIFICATION_TYPE } from '../constants/status';
+import { NOTIFICATION_TYPE, UPLOAD_IMAGES_STATUS } from '../constants/status';
 
 const noti = (type, message, description) => {
     notification[type]({
@@ -12,12 +12,27 @@ const noti = (type, message, description) => {
     });
 };
 const UploadImage = (props) => {
+    const [fileList, setFileList] = useState(props.defaultFileList);
+
+    useEffect(() => {
+        setFileList(props.defaultFileList);
+    }, [props.defaultFileList]);
+
     const onChange = async ({ file, fileList }) => {
         const isRemove = _.get(file, 'status', null);
-        if (isRemove === 'removed') return;
+        if (isRemove === UPLOAD_IMAGES_STATUS.REMOVED) return props.onChangeUrl(null);
         const formData = new FormData();
         formData.append('file', file);
         formData.append('upload_preset', 'web_app');
+
+        setFileList([
+            {
+                name: 'Đang tải ảnh lên',
+                url: file.name,
+                status: UPLOAD_IMAGES_STATUS.UPLOADING,
+                thumbUrl: file.name,
+            },
+        ]);
 
         const { data } = await axios({
             url: import.meta.env.VITE_URL_UPLOAD_IMAGE_CLOU,
@@ -27,27 +42,30 @@ const UploadImage = (props) => {
             },
             data: formData,
         });
+
         const urlCloudinary = _.get(data, 'url', null);
         if (urlCloudinary) {
-            props.onSendUrl(urlCloudinary);
+            props.onChangeUrl(urlCloudinary);
             return;
         }
+        props.onChangeUrl(null);
         noti(NOTIFICATION_TYPE.ERROR, 'Tải ảnh lên thất bại', 'Đã có lỗi xảy ra vui lòng thử lại!');
     };
 
     return (
-        <>
+        <div className="py-2">
             <Upload
                 action={import.meta.env.VITE_URL_UPLOAD_IMAGE_CLOU}
                 accept="image/png, image/jpeg"
-                maxCount={props.multiple ? null : 1}
+                maxCount={1}
                 listType="picture"
+                fileList={fileList || []}
                 onChange={onChange}
                 beforeUpload={() => false} //return false so that antd not upload image now
             >
-                <Button icon={<UploadOutlined />}>Upload</Button>
+                <Button icon={<UploadOutlined />}>Tải ảnh lên</Button>
             </Upload>
-        </>
+        </div>
     );
 };
 export default UploadImage;
