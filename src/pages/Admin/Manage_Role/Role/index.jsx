b810c0ff1937544f54f3d586ff6de05cd9bd08da
huynguyen <77097075+huynguyen0353080812@users.noreply.github.com>
2023-04-04 21:45:45 +0700

    feauter:filters/Role (#107)

--- src/pages/Admin/Manage_Role/Role/index.jsx
+++ src/pages/Admin/Manage_Role/Role/index.jsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Space, Table, Tag, Spin } from 'antd';
-import { EditOutlined, PlusOutlined } from '@ant-design/icons';
+import { Space, Table, Tag, Spin, Button, Tooltip } from 'antd';
+import { EditOutlined, PlusOutlined, SyncOutlined } from '@ant-design/icons';
 import DrawerCreateRole from './DrawerCreateRole';
 import { getAllRoleAsync } from '../../../../slices/role';
 import { isEmpty } from 'lodash';
+import Filter from '../../../../components/Filter/Filter';
 const index = () => {
     const [open, setOpen] = useState(false);
     const [action, setAction] = useState('');
     const [data, setData] = useState([]);
+    const [showroomsFilter, setShowroomsFilter] = useState([]);
     const [id, setId] = useState('');
     const Role = useSelector((state) => state.role.valueRole);
     const Loading = useSelector((state) => state.role.loading);
+    const datas = useRef([]);
     const dispatch = useDispatch();
     const OpenShowDrawer = () => {
         setOpen(true);
@@ -54,20 +57,52 @@ const index = () => {
                     tags: ['cool', 'teacher'],
                 };
             });
-            const newData = showdata.filter((item)=>item.name !== 'Admin')
+            const newData = showdata.filter((item) => item.name !== 'Admin');
+            datas.current = newData;
             setData(newData);
+            setShowroomsFilter(newData.map((item) => ({ label: item.name, value: item.key })));
         })();
     }, [Role]);
+    const handleFilter = (values) => {
+        if (values.nameId.length == 0) {
+            setData(datas.current);
+        } else {
+            setData(datas.current.filter((item) => values.nameId.includes(item.key)));
+        }
+    };
+    const handleFilters = () => {
+        setData(datas.current);
+    };
     return (
         <div>
-            <button
-                type="button"
-                onClick={() => OpenShowDrawer()}
-                className="focus:outline-none h-10 text-white bg-[#02b875] hover:bg-[#09915f] focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-base px-5 py-2.5 my-5"
-            >
-                <PlusOutlined className="pr-2 text-white " />
-                Thêm
-            </button>
+            <div className="flex justify-between">
+                <div>
+                    <Button onClick={() => OpenShowDrawer()} className="btn-primary text-white mr-5" type="primary">
+                        Thêm vai trò
+                    </Button>
+                    <button className="pr-6" onClick={() => handleFilters()}>
+                        <Tooltip title="Làm mới đơn hàng">
+                            <SyncOutlined style={{ fontSize: '18px', color: '#000' }} />
+                        </Tooltip>
+                    </button>
+                    <Filter
+                        items={[
+                            {
+                                label: <Space align="center">Tên Quyền</Space>,
+                                key: 'nameId',
+                                type: 'select',
+                                mode: 'multiple',
+                                values:showroomsFilter,
+                                name: 'Tên Quyền....',
+                            },
+                        ]}
+                        onFilter={handleFilter}
+                    />
+                </div>
+                <p className="p-5">
+                    Số lượng: <span className="font-bold">{data?.length}</span>
+                </p>
+            </div>
             <>
                 <Spin spinning={Loading}>
                     <Table columns={columns} dataSource={data} />;
